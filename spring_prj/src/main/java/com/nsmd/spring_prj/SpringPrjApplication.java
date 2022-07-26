a7c50b09911214e298dc59596e7c63c5a0e54efc
kitutune <p.kitutune@gmail.com>
2022-07-26 12:36:36 +0900

    upload

---
+++ spring_prj/src/main/java/com/nsmd/spring_prj/SpringPrjApplication.java
@@ -0,0 +1,13 @@
+package com.nsmd.spring_prj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPrjApplication.class, args);
+	}
+
+}
---
+++ spring_prj/src/test/java/com/nsmd/spring_prj/SpringPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsmd.spring_prj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
